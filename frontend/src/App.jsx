1d078a7feddf37c0cff4df38f6c2e7700bc091dc
Pranay Kothari <pranay.kothari109@gmail.com>
2024-03-16 07:42:18 +0530

    add input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
+import Input from "./components/input"
 
 function App() {
 
+  const [name, setName] = useState('');
+
   return (
-    <div className="text-xl p-3">Hello world</div>
+    <div>
+      <div className="text-xl p-3">Home Page!</div>
+      <div className="p-3 w-[30rem]">
+        <Input
+          name="name"
+          label="label"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/input/index.jsx
+++ frontend/src/components/input/index.jsx
@@ -1,6 +1,32 @@
-const Input = () => {
+const Input = (props) => {
+
+  const {
+    label,
+    type = 'text',
+    className,
+    value,
+    name,
+    onChange
+  } = props;
+
   return (
-    <div>Input</div>
+    <div className="space-y-1">
+      {label ? <label
+         className="text-[14px]"
+      >{label}</label> : null}
+      <div>
+        <input
+          type={type}
+          name={name}
+          value={value}
+          className={className ?
+            className :
+            'h-[54px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          }
+          onChange={onChange}
+        />
+      </div>
+    </div>
   )
 }
 
